f268a5e1f38f6b736af6b70b7f642c9559a22534
Kevin Duh <kevinduh@cs.jhu.edu>
2023-02-23 14:38:12 -0500

    add instructions for ASHA

--- automl/submit_run_automl.sh
+++ automl/submit_run_automl.sh
@@ -1,11 +1,17 @@
-python run_asha.py  -r 1 \
+#!/bin/sh
+
+rootdir=/exp/xzhang/sockeye-recipes3/
+rundir=$rootdir/egs/asha/space1/run1
+
+python $rootdir/automl/run_asha.py \
+                    -r 1 \
                     -u 1 \
                     -R 6\
                     -p 2 \
                     -G 4 \
                     --timer-interval 90 \
-                    --workdir /exp/xzhang/sockeye-recipes3/egs/asha/space1/run1 \
-                    --job-log-dir /exp/xzhang/sockeye-recipes3/egs/asha/space1/run1/job_logs \
-                    --ckpt /exp/xzhang/sockeye-recipes3/egs/asha/space1/run1/ckpt.json \
-                    --multi-objective 
-                    #--resume-from-ckpt /exp/xzhang/sockeye-recipes3/egs/asha/space1/run1/ckpt.json
+                    --workdir $rootdir/egs/asha/space1/run1 \
+                    --job-log-dir $rundir/job_logs \
+                    --ckpt $rundir/ckpt.json \
+                    #--multi-objective \
+                    #--resume-from-ckpt $rundir/ckpt.json 
